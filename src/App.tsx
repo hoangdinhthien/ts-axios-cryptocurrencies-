d42bd1bd25d0e36c578404d4bb9a1ad35388808a
hoangdinhthien <111034450+hoangdinhthien@users.noreply.github.com>
2023-03-03 15:19:55 +0700

    generate-dropdown-list

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,42 @@ import CryptoSummary from './components/CryptoSummary';
 import { Crypto } from './Types';
 
 function App() {
-  const [cryptos, setCryptos] = useState<Crypto[] | null>(null);
+    const [cryptos, setCryptos] = useState<Crypto[] | null>(null);
+    const [selected, setSelected] = useState<Crypto | null>();
 
-  useEffect(() => {
-    const url =
-      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=vnd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
-    axios.get(url).then((response) => {
-      setCryptos(response.data);
-    });
-  }, []);
+    useEffect(() => {
+        const url =
+            'https://api.coingecko.com/api/v3/coins/markets?vs_currency=vnd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
+        axios.get(url).then((response) => {
+            setCryptos(response.data);
+        });
+    }, []);
 
-  return (
-    <div className='App'>
-      {cryptos
-        ? cryptos.map((crypto) => {
-            return <CryptoSummary crypto={crypto}/>
-          })
-        : null}
-    </div>
-  );
+    return (
+        <>
+            <div className='App'>
+                <select
+                    onChange={(e) => {
+                        const c = cryptos?.find((x) => x.id === e.target.value);
+                        setSelected(c);
+                    }}
+                    defaultValue='default'
+                >
+                    <option value='default'>Choose an option:</option>
+                    {cryptos
+                        ? cryptos.map((crypto) => {
+                              return (
+                                  <option key={crypto.id} value={crypto.id}>
+                                      {crypto.name}
+                                  </option>
+                              );
+                          })
+                        : null}
+                </select>
+            </div>
+            {selected ? <CryptoSummary crypto={selected} /> : null}
+        </>
+    );
 }
 
 export default App;
