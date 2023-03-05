f10e63d40250bd7b191dd9f0cf255b426bc4a36d
hoangdinhthien <111034450+hoangdinhthien@users.noreply.github.com>
2023-03-06 00:34:15 +0700

    Dynamic Chart with Multiple Drop Downs

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,49 @@ import './App.css';
 import axios from 'axios'; //similar to fetch but cleaner
 import CryptoSummary from './components/CryptoSummary';
 import { Crypto } from './Types';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import type { ChartData, ChartOptions } from 'chart.js';
+import moment from 'moment';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
 
 function App() {
     const [cryptos, setCryptos] = useState<Crypto[] | null>(null);
     const [selected, setSelected] = useState<Crypto | null>();
+
+    const [range, setRange] = useState<number>(30);
+
+    const [data, setData] = useState<ChartData<'line'>>();
+    const [options, setOptions] = useState<ChartOptions<'line'>>({
+        responsive: true,
+        plugins: {
+            legend: {
+                display: false,
+            },
+            title: {
+                display: true,
+                text: 'Chart.js Line Chart',
+            },
+        },
+    });
 
     useEffect(() => {
         const url =
@@ -16,6 +55,55 @@ function App() {
         });
     }, []);
 
+    useEffect(() => {
+        if (!selected) return;
+        axios
+            .get(
+                `https://api.coingecko.com/api/v3/coins/${
+                    selected?.id
+                }/market_chart?vs_currency=vnd&days=${range}&${
+                    range === 1 ? 'interval=hourly' : `interval=daily`
+                }`
+            )
+            .then((response) => {
+                console.log(response.data);
+                setData({
+                    labels: response.data.prices.map((price: number[]) => {
+                        return moment
+                            .unix(price[0] / 1000)
+                            .format(range === 1 ? 'HH:MM' : 'MM-DD-YYYY');
+                    }),
+                    datasets: [
+                        {
+                            label: 'Dataset 1',
+                            data: response.data.prices.map(
+                                (price: number[]) => {
+                                    return price[1];
+                                }
+                            ),
+                            borderColor: 'rgb(255, 99, 132)',
+                            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                        },
+                    ],
+                });
+                setOptions({
+                    responsive: true,
+                    plugins: {
+                        legend: {
+                            display: false,
+                        },
+                        title: {
+                            display: true,
+                            text:
+                                `${selected?.name} Price over last ` +
+                                range +
+                                (range === 1 ? ` Day` : ` Days`),
+                        },
+                    },
+                });
+            });
+    }, [selected, range]);
+
     return (
         <>
             <div className='App'>
@@ -23,6 +111,9 @@ function App() {
                     onChange={(e) => {
                         const c = cryptos?.find((x) => x.id === e.target.value);
                         setSelected(c);
+
+                        // made a new request
+                        // and then update the data state
                     }}
                     defaultValue='default'
                 >
@@ -37,8 +128,25 @@ function App() {
                           })
                         : null}
                 </select>
+
+                <select
+                    onChange={(e) => {
+                        setRange(parseInt(e.target.value));
+                    }}
+                >
+                    <option value={30}>30 days</option>
+                    <option value={7}>7 days</option>
+                    <option value={1}>1 day</option>
+                </select>
             </div>
             {selected ? <CryptoSummary crypto={selected} /> : null}
+
+            {/* if data exist(have a value) => display <Line/>, otherwise => null */}
+            {data ? (
+                <div style={{ width: 600 }}>
+                    <Line options={options} data={data} />
+                </div>
+            ) : null}
         </>
     );
 }
